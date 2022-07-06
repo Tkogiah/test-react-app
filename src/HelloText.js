c8e0daeaecaf9bae80cc75a5d5cbcfc826482ae5
Marcus Overstreet <tkogiah@gmail.com>
2022-07-06 05:06:08 -0700

    played around with stuff

---
+++ src/HelloText.js
@@ -0,0 +1,24 @@
+function HelloText() {
+    return (
+        <div>
+            <h1>Hello Marcus!</h1>
+            <h2>hello text</h2>
+        </div>   
+    )
+}
+function HelloTextTwo() {
+    return [
+            <h1>Hello Marcus!</h1>,
+            <h2>Hello text two</h2>  
+    ]
+}
+function HelloTextThree() {
+    return (
+        <div>
+            <h1>Hello Marcus!</h1>
+            <h2>YOU DID IT!!!</h2>
+        </div>   
+    )
+}
+
+export default HelloText
