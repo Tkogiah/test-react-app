624aaa0a35020b443a7e7ade6ea723286c545ac1
Marcus Overstreet <tkogiah@gmail.com>
2022-07-07 08:15:48 -0700

    messing with useState React function

--- src/App.js
+++ src/App.js
@@ -1,12 +1,25 @@
 import './App.css';
 import HelloText from './HelloText'
+import React from 'react';
 
-function App() {
+export default function App() {
+
+  const[number, setNumber] = React.useState(0);
+  const[textValue, setTextValue] = React.useState('');
+  //const[number, setNumber] = React.useState(0);
+  
+
   return (
     <div className="App">
       <HelloText />
+      <h2>{number}</h2>
+      <button onClick={() => setNumber(number + 1)}>
+        +
+      </button>
+      <input onChange={e => setTextValue(e.target.value)} type='text' />
+      <h2>{textValue}</h2>
+      {textValue.length > 10 && <h3>too many caharcters buddy</h3>}
     </div>
   );
 }
 
-export default App;
